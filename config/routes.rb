191c954ac7a4a92f7a0d8c0a77832cdd318c0368
fannyibanez <fannyibanez89@gmail.com>
2020-11-10 15:57:54 +0100

    Routes done

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,15 @@ Rails.application.routes.draw do
 
   get "tasks/new", to: "tasks#new"
 
+  post "tasks", to: "tasks#create"
+
+  get  "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+
+  patch "tasks/:id", to: "tasks#update"
+
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  delete "tasks/:id", to: "tasks#destroy"
+
+
 end
